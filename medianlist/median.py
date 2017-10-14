abc7b4c4161ef78f4e0a1ff3866ad2bb8058c9eb
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-10-15 07:18:34 +0800

    refined test cases

--- medianlist/median.py
+++ medianlist/median.py
@@ -38,24 +38,22 @@ class MedianList(object):
 
 def main():
     testcases = [
-        [[1, 3, 9, 5, 7, 5, 0, 4, 3, 2, 9, 1, 0], 3],
-        [[6, 1, 2], 2],
-        [[3, 9, 2, 5, 2], 3],
-        [[3, 9, 2, 5, 2], 3],
-        [[5, 5, 5, 5], 5],
-        [[9, 6, 4, 3, 1], 4],
-        [[1, 2, 3, 4], 2.5],
-        [[1, 3, 2], 2],
-        [[1], 1],
-        [[], 0]
+        [[1, 3, 9, 5, 7, 5, 0, 4, 3, 2, 9, 1, 0], [1, 2, 3, 4, 5, 5, 5, 4.5, 4, 3.5, 4, 3.5, 3]],
+        [[6, 1, 2], [6, 3.5, 2]],
+        [[3, 9, 2, 5, 2], [3, 6, 3, 4, 3]],
+        [[5, 5, 5, 5], [5, 5, 5, 5]],
+        [[9, 6, 4, 3, 1], [9, 7.5, 6, 5, 4]],
+        [[1, 2, 3, 4], [1, 1.5, 2, 2.5]],
+        [[1, 3, 2], [1, 2, 2]],
+        [[1], [1]]
     ]
 
     for case in testcases:
         median_list = MedianList()
         print('Executing test case %s' % case[0])
-        for number in case[0]:
+        for k, number in enumerate(case[0]):
             median_list.add_num(number)
-        assert median_list.get_median() == case[1]
+            assert median_list.get_median() == case[1][k]
 
 if __name__ == '__main__':
     main()
