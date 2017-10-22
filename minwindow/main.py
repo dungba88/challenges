dddd7250c5dee1fc031a0b889c3b8cb944615531
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-10-22 17:36:48 +0800

    min window substring

---
+++ minwindow/main.py
@@ -0,0 +1,17 @@
+from min_window import min_window
+
+def main():
+    testcases = [
+        ["ADOBECODEBANC", "ABC", "BANC"],
+        ["ADOBECODEBANC", "ABBC", "BECODEBA"],
+        ["ABC", "A", "A"],
+        ["ABC", "X", None],
+        ["AXXBXBXAXXAC", "AB", "BXA"],
+    ]
+
+    for case in testcases:
+        print('Executing test case %s - %s' % (case[0], case[1]))
+        assert min_window(case[0], case[1]) == case[2]
+
+if __name__ == '__main__':
+    main()
---
+++ minwindow/min_window.py
@@ -0,0 +1,40 @@
+import math
+
+def min_window(s, t):
+    n = len(s)
+
+    cur_min_head = None
+    cur_min = math.inf
+
+    fulfilled_map = {}
+    required_map = {}
+    for c in t:
+        fulfilled_map[c] = 1
+        required_map[c] = 1 if c not in required_map else required_map[c] + 1
+
+    i = j = 0
+    last_j = -1
+
+    while i < n and j < n:
+        head = s[i]
+        tail = s[j]
+        if tail in required_map and last_j != j:
+            last_j = j
+            required_map[tail] -= 1
+            if required_map[tail] == 0:
+                del fulfilled_map[tail]
+        if fulfilled_map:
+            j += 1
+        else:
+            if cur_min > j - i:
+                cur_min = j - i
+                cur_min_head = i
+            if head in required_map:
+                required_map[head] += 1
+                if required_map[head] > 0:
+                    fulfilled_map[head] = 1
+            i += 1
+
+    if cur_min == math.inf:
+        return None
+    return s[cur_min_head:cur_min_head + cur_min + 1]
