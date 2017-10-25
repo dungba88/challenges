161c7f36bedb9b778e916141a7aec426f9df213b
Dzung Bui <dungba.sg@gmail.com>
2017-10-25 22:23:54 +0800

    refactor

--- medianlist/median.py
+++ medianlist/median.py
@@ -35,25 +35,3 @@ class MedianList(object):
             else:
                 high = idx - 1
         return 0
-
-def main():
-    testcases = [
-        [[1, 3, 9, 5, 7, 5, 0, 4, 3, 2, 9, 1, 0], [1, 2, 3, 4, 5, 5, 5, 4.5, 4, 3.5, 4, 3.5, 3]],
-        [[6, 1, 2], [6, 3.5, 2]],
-        [[3, 9, 2, 5, 2], [3, 6, 3, 4, 3]],
-        [[5, 5, 5, 5], [5, 5, 5, 5]],
-        [[9, 6, 4, 3, 1], [9, 7.5, 6, 5, 4]],
-        [[1, 2, 3, 4], [1, 1.5, 2, 2.5]],
-        [[1, 3, 2], [1, 2, 2]],
-        [[1], [1]]
-    ]
-
-    for case in testcases:
-        median_list = MedianList()
-        print('Executing test case %s' % case[0])
-        for k, number in enumerate(case[0]):
-            median_list.add_num(number)
-            assert median_list.get_median() == case[1][k]
-
-if __name__ == '__main__':
-    main()
