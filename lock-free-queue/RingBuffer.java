21f59a0468aa4ce1d2f52fa2baaa8acde1720e25
Dzung Bui <dungba.sg@gmail.com>
2017-11-02 22:41:58 +0800

    refactor

--- lock-free-queue/RingBuffer.java
+++ lock-free-queue/RingBuffer.java
@@ -2,37 +2,42 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RingBuffer implements LockFreeQueue<Integer> {
 
+	private int mask;
+
+	private int maxSize;
+
 	private volatile int[] data;
-	
+
 	private volatile int start;
-	
+
 	private volatile int length;
-	
+
 //	private SpinLock lock = new SpinLock();
 	
 	private AtomicBoolean lock = new AtomicBoolean(false);
-	
+
 	public RingBuffer(int maximumSize) {
 		if (!isPowerOf2(maximumSize)) {
 			throw new RuntimeException("Maximum size must be power of 2");
 		}
 		data = new int[maximumSize];
+		mask = maximumSize - 1;
+		maxSize = maximumSize;
 		start = length = 0;
 	}
-	
+
 	private boolean isPowerOf2(int maximumSize) {
 		return (maximumSize & (maximumSize - 1)) == 0;
 	}
 
 	public boolean add(Integer number) {
 		if (isFull()) return false;
-//		lock.lock();
 		while(!lock.compareAndSet(false, true)) {}
 		try {
 			if (isFull()) return false;
-			data[mask(start + length++)] = number;
+			data[(start + length) & mask] = number;
+			length++;
 		} finally {
-//			lock.unlock();
 			lock.set(false);
 		}
 		return true;
@@ -40,41 +45,34 @@ public class RingBuffer implements LockFreeQueue<Integer> {
 
 	public Integer poll() {
 		if (isEmpty()) return null;
-//		lock.lock();
 		while(!lock.compareAndSet(false, true)) {}
 		try {
 			if (isEmpty()) return null;
 			int result = data[start];
-			start = mask(start + 1);
+			start = (start + 1) & mask;
 			length--;
 			return result;
 		} finally {
-//			lock.unlock();
 			lock.set(false);
 		}
 	}
-	
+
 	public boolean isFull() {
-		return length == data.length;
+		return length == maxSize;
 	}
 
 	public boolean isEmpty() {
 		return length == 0;
-	}
-	
-	private int mask(int i) {
-		if (i < data.length)
-			return i;
-		return i & (data.length - 1);
 	}
 }
 
 class SpinLock {
 
 	private AtomicBoolean lock = new AtomicBoolean(false);
-	
+
 	public void lock() {
-		while(!lock.compareAndSet(false, true)) {}
+		while (!lock.compareAndSet(false, true)) {
+		}
 	}
 
 	public void unlock() {
